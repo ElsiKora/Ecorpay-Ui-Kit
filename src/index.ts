34dad23274d9038ba462947a7eef23e1cdcfdea7
alexanderElsikora <alexander.frontend@elsikora.com>
2025-01-23 19:50:20 +0700

    fix(config): Added Tooltip to libs public api

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import './assets/fonts.css';
 import './assets/style.css';
 
-export { Slot, type SlotProps } from '@radix-ui/react-slot';
-export { Button, type ButtonProps } from './shared/Button/Button';
 export { Banner, type BannerProps } from './shared/Banner/Banner';
+export { Button, type ButtonProps } from './shared/Button/Button';
 export { Checkbox, type CheckboxProps } from './shared/Checkbox/Checkbox';
 export { CopyButton, type CopyButtonProps } from './shared/CopyButton/CopyButton';
 export { CopyInput, type CopyInputProps } from './shared/CopyInput/CopyInput';
 export { Input, type InputProps } from './shared/Input/Input';
 export { Radio, type RadioProps } from './shared/Radio/Radio';
+export { Skeleton, type SkeletonProps } from './shared/Skeleton/Skeleton';
 export { Switch, type SwitchProps } from './shared/Switch/Switch';
 export { Toast, type ToastProps } from './shared/Toast/Toast';
-export { Skeleton, type SkeletonProps } from './shared/Skeleton/Skeleton';
+export { Tooltip, type TooltipProps } from './shared/Tooltip/Tooltip';
+
+export { Slot, type SlotProps } from '@radix-ui/react-slot';
 
 import tailwindConfig from '../tailwind.config';
 export { tailwindConfig };
