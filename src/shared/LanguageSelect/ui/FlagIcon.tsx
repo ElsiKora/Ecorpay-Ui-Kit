17b017c9b4323e402607629e4635a43b2ebe84f2
alexanderElsikora <alexander.frontend@elsikora.com>
2025-04-02 15:53:34 +0700

    fix(lang): Fix lang icons

--- src/shared/LanguageSelect/ui/FlagIcon.tsx
+++ src/shared/LanguageSelect/ui/FlagIcon.tsx
@@ -6,19 +6,22 @@ interface FlagIconProps {
   className?: string;
 }
 
+const BASE_URL =
+  process.env.NODE_ENV === 'development' ? '' : 'node_modules/@elsikora/ecorpay-ui-kit/dist';
+
 export const FlagIcon: FC<FlagIconProps> = ({ code, className }) => {
-  const flagSrc = `/icons/flags/${code.toLowerCase()}.svg`;
-  const fallback = `/icons/flags/fallback.svg`;
+  const flagSrc = `${BASE_URL}/icons/flags/${code.toLowerCase()}.svg`;
+  const fallback = `${BASE_URL}/icons/flags/fallback.svg`;
 
   return (
     <img
       className={cn('w-20px h-20px shrink-0 border-2 border-black-100 rounded-md', className)}
       alt={`${code} flag`}
-      src={new URL(flagSrc, import.meta.url).href}
+      src={flagSrc}
       width={22}
       height={22}
       onError={(e) => {
-        e.currentTarget.src = new URL(fallback, import.meta.url).href;
+        e.currentTarget.src = fallback;
       }}
     />
   );
