ac4758b61103bf1881889095cfd07c621637a6ab
alexanderElsikora <alexander.frontend@elsikora.com>
2025-04-15 19:13:19 +0700

    fix(css): reorganize css file structure and build process

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 
-import "../src/assets/globals.css";
+import "../src/globals.css";
 
 const preview: Preview = {
  parameters: {
--- rollup.config.js
+++ rollup.config.js
@@ -136,7 +136,7 @@ export default defineConfig([
   },
   plugins: [
    postcss({
-    extract: "css/fonts.css",
+    extract: "fonts.css",
     include: ["src/assets/fonts.css"],
     minimize: true,
     plugins: [autoprefixer()],
@@ -149,7 +149,7 @@ export default defineConfig([
   ],
  },
 
- // 4️⃣ Конфиг для theme.css
+ // Конфиг для theme.css
  {
   input: "src/assets/theme.css",
   output: {
@@ -161,7 +161,18 @@ export default defineConfig([
     extract: "css/theme.css",
     include: ["src/assets/theme.css"],
     minimize: true,
-    plugins: [autoprefixer()],
+    plugins: [
+     // Кастомный плагин для замены :root на @theme static
+     (css) => {
+      css.walkRules(/:root|:host,\s*:root/, (rule) => {
+       rule.selector = "@theme static";
+      });
+
+      return css;
+     },
+
+     autoprefixer(),
+    ],
     sourceMap: false,
    }),
    del({
